ef572592addc57be28221d2cef4154635e99f11c
kornel <k.furmanczyk@grinddev.com>
2025-03-31 19:46:46 +0200

    feat: fetching task detaisl and sending answer

--- tasks/01_helloapi.py
+++ tasks/01_helloapi.py
@@ -1,5 +1,8 @@
-from lib.handle_task import get_auth_token
+from lib.handle_task import get_auth_token, get_task_details, send_answer
 
 token = get_auth_token("helloapi")
+task_details = get_task_details(token)
 
-print(token)
+answer = task_details["cookie"]
+
+send_answer(token, answer)
