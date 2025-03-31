3a830152163230e649357343b69ab9d955cde125
kornel <k.furmanczyk@grinddev.com>
2025-03-31 19:07:42 +0200

    feat: token retrieval

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ai-devs2
 Python code for the AI-devs2 
+
+
+To run tasks:
+    python -m tasks.$task_name
---
+++ tasks/01_helloapi.py
@@ -0,0 +1,5 @@
+from lib.handle_task import get_auth_token
+
+token = get_auth_token("helloapi")
+
+print(token)
